2e0fb73c569e60cffbafd62a20f9e2b12b02ad2f
strongkid21 <1751862489@qq.com>
2022-09-28 20:01:49 +0800

    9.28提交
    角度解算增加基本的代码，具体弹道计算还需理论推导

--- include/AngleSolve.h
+++ include/AngleSolve.h
@@ -8,6 +8,7 @@
 #include "robot_state.h"
 #include "opencv2/opencv.hpp"
 #include <Eigen/Dense>
+#include "ArmorDetector.hpp"
 
 struct headAngle
 {
@@ -20,17 +21,39 @@ class AngleSolve : public robot_state
 public:
     AngleSolve();
 
-    void getAngle(Eigen::Vector3d &pos);
+    void getAngle(Armor &aimArmor);
+
+
 
 private:
+    double big_w;
+    double big_h;
+    double small_w;
+    double small_h;
+
     Eigen::Matrix<double,3,3> F_EGN;
     Eigen::Matrix<double,1,5> C_EGN;
+
     cv::Mat F_MAT;
     cv::Mat C_MAT;
 
-    double getGravity(Eigen::Vector3d &transedPos);
-
     headAngle send;
+
+    Eigen::Vector3d transformPos2_World(Eigen::Vector3d &Pos);
+
+    Eigen::Vector3d transformPos2_Camera(Eigen::Vector3d &Pos);
+
+    void pnpSolve(Point2f p[4], EnermyType type);
+
+    void gravitySolve(Eigen::Vector3d &Pos);
+
+    void airResistanceSolve(Eigen::Vector3d &Pos);
+
+    void yawPitchSolve(Eigen::Vector3d &Pos);
+
+
+
+
 };
 
 #endif //SHSIHI_ANGLESOLVE_H
--- src/AngleSolve.cpp
+++ src/AngleSolve.cpp
@@ -14,7 +14,23 @@ AngleSolve::AngleSolve()
     C_MAT=(Mat_<double>(1, 5) << -0.08424, 0.16737, -0.00006, 0.00014, 0.00000);
 }
 
-void AngleSolve::getAngle(Vector3d &pos)
+Vector3d AngleSolve::transformPos2_World(Vector3d &Pos)
+{
+    Mat camera2_tuoluo = (Mat_<double>(3,1) << CV_PI/2,0,0);
+    Mat eular = (Mat_<double>(3,1) << ab_pitch/180*CV_PI,ab_yaw/180*CV_PI,ab_roll/180*CV_PI);
+    Mat rotated_mat,coordinate_mat;
+    Rodrigues(camera2_tuoluo,coordinate_mat);
+    Rodrigues(eular,rotated_mat);
+
+    Matrix<double,3,3> rotated_matrix,coordinate_maxtrix;
+    cv2eigen(rotated_mat,rotated_matrix);
+    cv2eigen(coordinate_mat,coordinate_maxtrix);
+
+    return rotated_matrix*(coordinate_maxtrix*Pos);
+}
+
+
+void AngleSolve::getAngle(Armor &aimArmor)
 {
     ////sample////
     Vector3d po;
@@ -39,13 +55,65 @@ void AngleSolve::getAngle(Vector3d &pos)
     std::cout<<dst_po[2]<<std::endl;
     /////////////////////
 
-    getGravity(dst_po);
 }
 
-double AngleSolve::getGravity(Eigen::Vector3d &transedPos)
+void AngleSolve::gravitySolve(Vector3d &Pos)
 {
     double height;
 
 
 
+}
+
+void AngleSolve::pnpSolve(Point2f p[4], EnermyType type)
+{
+    double w = type == SMALL ? small_w : big_w;
+    double h = type == SMALL ? small_h : big_h;
+    cv::Point2f lu, ld, ru, rd;
+    std::vector<cv::Point3d> ps = {
+            {-w / 2 , -h / 2, 0.},
+            {w / 2 , -h / 2, 0.},
+            {w / 2 , h / 2, 0.},
+            {-w / 2 , h / 2, 0.}
+    };
+    if (p[0].y < p[1].y) {
+        lu = p[0];
+        ld = p[1];
+    }
+    else {
+        lu = p[1];
+        ld = p[0];
+    }
+    if (p[2].y < p[3].y) {
+        ru = p[2];
+        rd = p[3];
+    }
+    else {
+        ru = p[3];
+        rd = p[2];
+    }
+
+    std::vector<cv::Point2f> pu;
+    pu.push_back(lu);
+    pu.push_back(ru);
+    pu.push_back(rd);
+    pu.push_back(ld);
+
+    cv::Mat rvec;
+    cv::Mat tvec;
+    Eigen::Vector3d tv;
+
+
+    cv::solvePnP(ps, pu, F_MAT, C_MAT, rvec, tvec);
+
+
+    cv::cv2eigen(tvec, tv);
+}
+
+void AngleSolve::yawPitchSolve(Eigen::Vector3d &Pos)
+{
+    send.yaw = atan2(Pos(0,0) ,
+                     Pos(2,0)) / CV_PI*180.0 - ab_yaw;
+    send.pitch = atan2(Pos(1,0) ,
+                       sqrt(Pos(0,0)*Pos(0,0) + Pos(2,0)*Pos(2,0))) / CV_PI*180.0 - ab_pitch;
 }
