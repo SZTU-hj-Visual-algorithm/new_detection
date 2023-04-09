e056fe56abc32d0d5488f8d9d72c4afc06fac56e
Lmx6771 <2351006771@qq.com>
2023-04-09 09:41:37 +0800

    多线程修改

--- main.cpp
+++ main.cpp
@@ -11,14 +11,14 @@ using namespace cv;
 
 pthread_t thread1;
 pthread_t thread2;
-pthread_t thread3;
+//pthread_t thread3;
 
 pthread_mutex_t mutex_new;
 pthread_cond_t cond_new;
-pthread_mutex_t mutex_ka;
-pthread_cond_t cond_ka;
+//pthread_mutex_t mutex_ka;
+//pthread_cond_t cond_ka;
 
-bool is_ka = false;
+//bool is_ka = false;
 bool is_start = false;
 bool is_continue = true;
 
@@ -27,12 +27,12 @@ int main(void)
     XInitThreads();
     pthread_mutex_init(&mutex_new, NULL);
     pthread_cond_init(&cond_new, NULL);
-    pthread_create(&thread1, NULL, Build_Src, NULL);
-    pthread_create(&thread2, NULL, Armor_Kal, NULL);
-    pthread_create(&thread3, NULL, Kal_predict, NULL);
+    pthread_create(&thread1, NULL, Sample, NULL);
+    pthread_create(&thread2, NULL, Implement, NULL);
+//  pthread_create(&thread3, NULL, Send, NULL);
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
-    pthread_join(thread3, NULL);
+//  pthread_join(thread3, NULL);
     pthread_mutex_destroy(&mutex_new);
     return 0;
 }
